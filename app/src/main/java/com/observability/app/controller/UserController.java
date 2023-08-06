bc614282e363396120370f24fbfe47781398adbf
Krishna Chaitanya Surapaneni <chaitanya1004@gmail.com>
2023-08-06 14:19:25 -0600

    HelloController changed to UserController and a GET end-point is added to fetch user info

--- app/src/main/java/com/observability/app/controller/UserController.java
+++ app/src/main/java/com/observability/app/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.observability.app.controller;
 
+import com.observability.app.dto.*;
+import com.observability.app.service.*;
 import org.slf4j.*;
 import org.springframework.web.bind.annotation.*;
 
@@ -7,19 +9,23 @@ import org.springframework.web.bind.annotation.*;
  * @author Krishna Chaitanya
  */
 @RestController
-@RequestMapping("/hello")
-public class HelloController {
+@RequestMapping("/user")
+public class UserController {
 
-    private static final Logger logger = LoggerFactory.getLogger(HelloController.class);
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 
     @GetMapping
-    Hello getMessage(@RequestParam String name) {
-        logger.info("getMessage() is called");
-        return new Hello("Hello, " + name);
+    User getUser(@RequestParam int id) {
+        logger.info("getUser() is called");
+        logger.info("calling userService.fetchUser(id)");
+        return userService.fetchUser(id);
     }
 
-
-    record Hello(String text) {
-    }
 
 }
