357f4d5f4c7b732e8b4aa40cba30dd80684cc2fe
Krishna Chaitanya Surapaneni <chaitanya1004@gmail.com>
2023-08-07 10:49:20 -0600

    Configuration is added - RestTemplate bean has been created(which is helpful to maintain the same traceid when calling other service)

---
+++ app/src/main/java/com/observability/app/configuration/AppConfiguration.java
@@ -0,0 +1,20 @@
+package com.observability.app.configuration;
+
+import org.springframework.boot.web.client.*;
+import org.springframework.context.annotation.*;
+import org.springframework.web.client.*;
+
+/**
+ * @author Krishna Chaitanya
+ */
+@Configuration
+public class AppConfiguration {
+
+    // It is important to use RestTemplateBuilder when building the restTemplate
+    // for propagating trace id from one api to another
+    @Bean
+    RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
+    }
+
+}
